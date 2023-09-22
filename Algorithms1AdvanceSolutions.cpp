c12bb0f9f51632b78f2d536b8888dd39d680d258
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-22 18:05:02 +0300

    2#: Read & Print name

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -3,11 +3,22 @@
 
 using namespace std;
 
+string readName()
+{
+    string name;
+
+    cout << "Please Enter your name: \n";
+    getline(cin, name);
+
+    return name;
+}
+
 void printName(string name)
 {
     cout << "Your name is: " << name << endl;
 }
+
 int main()
 {
-    printName("Abdulmajeed");
+    printName(readName());
 }
