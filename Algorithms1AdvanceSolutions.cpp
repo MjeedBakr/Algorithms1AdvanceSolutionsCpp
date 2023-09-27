6d10f3f70f47192df2806d9d423748c3f3629565
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-27 08:39:45 +0300

    9#: average of 3 numbers

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -17,14 +17,19 @@ int sumOf3Numbers(int num1, int num2, int num3)
 	return num1 + num2 + num3;
 }
 
+int averageOf3Numbers(int num1, int num2, int num3)
+{
+	return (float)sumOf3Numbers(num1, num2, num3) / 3;
+}
+
 void printResult(int sum)
 {
-	cout << "\nThe sum of 3 numbers = " << sum << endl;
+	cout << "\nThe average of 3 numbers = " << sum << endl;
 }
 
 int main()
 {
 	int num1, num2, num3;
 	readNumbers(num1, num2, num3);
-	printResult(sumOf3Numbers(num1, num2, num3));
+	printResult(averageOf3Numbers(num1, num2, num3));
 }
