6221176626f4fdedfa4fed8e2e845ceb2a7fbc21
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-28 14:31:42 +0300

    17#: calculate triangle area

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -2,29 +2,29 @@
 
 using namespace std;
 
-void readNumbers(float &s, float &d)
+void readNumbers(float &a, float &h)
 {
-	cout << "Enter rectangle side S: ";
-	cin >> s;
-	cout << "Enter rectangle diagonal D: ";
-	cin >> d;
+	cout << "Enter triangle base a: ";
+	cin >> a;
+	cout << "Enter triangle height h: ";
+	cin >> h;
 }
 
-float calcRectangleAreaBySideAndDiagonal(float side, float diagonal)
+float calcTrangleArea(float base, float height)
 {
-	float area = side * sqrt(pow(d, 2) - pow(a, 2));
+	float area = (base/2) * height;
 	return area;
 }
 
 void printNumbers(float area)
 {
-	cout << "Rectangle Area = " << area << endl;
+	cout << "Triangle Area = " << area << endl;
 }
 
 int main()
 {
 	float num1, num2;
 	readNumbers(num1, num2);
-	printNumbers(calcRectangleAreaBySideAndDiagonal(num1, num2));
+	printNumbers(calcTrangleArea(num1, num2));
 
 }
