2f7bb592dc387d0b4833cb303ca7ac66b6cd1b26
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-30 15:05:18 +0300

    29#: sum even numbers form 1 to N

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -20,44 +20,44 @@ enOddOrEven checkOddOrEven(int number)
 		return enOddOrEven::Odd;
 }
 
-int sumOddNumbersFrom1ToN_usingWhile(int N)
+int sumEvenNumbersFrom1ToN_usingWhile(int N)
 {
 	int counter = 0;
 	int sum = 0;
 
-	cout << "Sum using while statement: \n";
+	cout << "Sum even numbers using while statement: \n";
 	while (counter < N)
 	{
 		counter++;
-		if (checkOddOrEven(counter) == enOddOrEven::Odd)
+		if (checkOddOrEven(counter) == enOddOrEven::Even)
 			sum += counter;
 	}
 	return sum;
 }
 
-int sumOddNumbersFrom1ToN_usingDoWhile(int N)
+int sumEvenNumbersFrom1ToN_usingDoWhile(int N)
 {
 	int counter = 0;
 	int sum = 0;
-	cout << "sum using do..while statement: \n";
+	cout << "sum even numbers using do..while statement: \n";
 
 	do
 	{
 		counter++;
-		if (checkOddOrEven(counter) == enOddOrEven::Odd)
+		if (checkOddOrEven(counter) == enOddOrEven::Even)
 			sum += counter;
 
 	} while (counter < N);
 	return sum;
 }
 
-int sumOddNumbersFrom1ToN_usingFor(int N)
+int sumEvenNumbersFrom1ToN_usingFor(int N)
 {
-	cout << "sum using for loop statement: \n";
+	cout << "sum even number using for loop statement: \n";
 	int sum = 0;
 	for (int counter = 1; counter <= N; counter++)
 	{
-		if (checkOddOrEven(counter) == enOddOrEven::Odd)
+		if (checkOddOrEven(counter) == enOddOrEven::Even)
 			sum += counter;
 	}
 	return sum;
@@ -67,9 +67,9 @@ int main()
 {
 	int N = readNumber();
 
-	cout << sumOddNumbersFrom1ToN_usingWhile(N) << endl;
-	cout << sumOddNumbersFrom1ToN_usingDoWhile(N) << endl;
-	cout << sumOddNumbersFrom1ToN_usingFor(N) << endl;
+	cout << sumEvenNumbersFrom1ToN_usingWhile(N) << endl;
+	cout << sumEvenNumbersFrom1ToN_usingDoWhile(N) << endl;
+	cout << sumEvenNumbersFrom1ToN_usingFor(N) << endl;
 
 
 	return 0;
