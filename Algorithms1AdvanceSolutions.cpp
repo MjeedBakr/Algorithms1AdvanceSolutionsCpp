6ccc2b6c8ea5096a2580ac1c0808dccb3ae3c1e5
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-30 15:14:22 +0300

    30#: factorial of N

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -2,74 +2,32 @@
 #include <string>
 using namespace std;
 
-enum enOddOrEven {Odd = 1, Even = 2};
-int readNumber()
+int readPositiveNumber(string message)
 {
 	int number;
-	cout << "Please enter your number: ";
+	do
+	{
+	cout << message;
 	cin >> number;
+	} while (number < 0);
 
 	return number;
 }
 
-enOddOrEven checkOddOrEven(int number)
+int factorial(int N)
 {
-	if (number % 2 == 0)
-		return enOddOrEven::Even;
-	else
-		return enOddOrEven::Odd;
+	int F = 1;
+	for (int counter = N; counter >= 1; counter--)
+		F = F * counter;
+
+	return F;
 }
 
-int sumEvenNumbersFrom1ToN_usingWhile(int N)
-{
-	int counter = 0;
-	int sum = 0;
 
-	cout << "Sum even numbers using while statement: \n";
-	while (counter < N)
-	{
-		counter++;
-		if (checkOddOrEven(counter) == enOddOrEven::Even)
-			sum += counter;
-	}
-	return sum;
-}
-
-int sumEvenNumbersFrom1ToN_usingDoWhile(int N)
-{
-	int counter = 0;
-	int sum = 0;
-	cout << "sum even numbers using do..while statement: \n";
-
-	do
-	{
-		counter++;
-		if (checkOddOrEven(counter) == enOddOrEven::Even)
-			sum += counter;
-
-	} while (counter < N);
-	return sum;
-}
-
-int sumEvenNumbersFrom1ToN_usingFor(int N)
-{
-	cout << "sum even number using for loop statement: \n";
-	int sum = 0;
-	for (int counter = 1; counter <= N; counter++)
-	{
-		if (checkOddOrEven(counter) == enOddOrEven::Even)
-			sum += counter;
-	}
-	return sum;
-}
 
 int main()
 {
-	int N = readNumber();
-
-	cout << sumEvenNumbersFrom1ToN_usingWhile(N) << endl;
-	cout << sumEvenNumbersFrom1ToN_usingDoWhile(N) << endl;
-	cout << sumEvenNumbersFrom1ToN_usingFor(N) << endl;
+	cout << "Factorial = " << factorial(readPositiveNumber("Enter a positive number: "));
 
 
 	return 0;
