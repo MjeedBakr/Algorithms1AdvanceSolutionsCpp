53fb87614c0f15d35dd6f5910191884642511094
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-09-30 13:33:21 +0300

    23#: calculate circle area by arbitary triangle

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -2,19 +2,26 @@
 #include <string>
 using namespace std;
 
-void readTriangleData(float& a, float& b)
+void readTriangleData(float& a, float& b, float& c)
 {
 	cout << "Please enter triangle side A: ";
 	cin >> a;
 	cout << "Please enter triangle Base B: ";
 	cin >> b;
+	cout << "Please enter triangle side C: ";
+	cin >> c;
 }
 
-float circleAreaByITriangle(float a, float b)
+float circleAreaByATriangle(float a, float b, float c)
 {
 	const float PI = 3.141592653589793238;
+	float P = (a + b + c) / 2;
 
-	float area = PI * (pow(b, 2) / 4) * ((2 * a - b) / (2 * a + b));
+	float T;
+	T = (a * b * c) / (4 * sqrt(P * (P - a) * (P - b) * (P - c)));
+
+
+	float area = PI * pow(T, 2);
 	return area;
 }
 
@@ -25,9 +32,9 @@ void printArea(float area)
 
 int main()
 {
-	float a, b;
-	readTriangleData(a, b);
-	printArea(circleAreaByITriangle(a, b));
+	float a, b, c;
+	readTriangleData(a, b, c);
+	printArea(circleAreaByATriangle(a, b, c));
 
 	return 0;
 }
