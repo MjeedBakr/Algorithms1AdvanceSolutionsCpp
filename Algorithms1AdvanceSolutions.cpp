34cafbc09e17ba92669154bbb91ebbf56073e71e
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-02 14:48:53 +0300

    45#: month of year

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -2,7 +2,9 @@
 #include <string>
 using namespace std;
 
-enum enWeekDays {Sat = 1, Sun = 2, Mon = 3, Tue = 4, Wed = 5, Thu = 6, Fri = 7};
+enum enMonthOfYear {Jan = 1, Feb = 2, Mar = 3, Apr = 4, May = 5,
+					Jun = 6, Jul = 7, Aug = 8, Sep = 9, Oct = 10,
+					Nov = 11, Dec = 12};
 
 int readNumberInRange(string message, int start, int end)
 {
@@ -16,38 +18,48 @@ int readNumberInRange(string message, int start, int end)
 	return number;
 }
 
-enWeekDays readDayOfWeek()
+enMonthOfYear readMonthOfYear()
 {
-	return (enWeekDays)readNumberInRange("Please enter day number: ", 1, 7);
+	return (enMonthOfYear)readNumberInRange("Please enter month number 1-12: ", 1, 12);
 }
 
-string getDayOfWeek(enWeekDays day)
+string getMonthOfYear(enMonthOfYear month)
 {
-	switch (day)
+	switch (month)
 	{
-	case Sat:
-		return "Saturady";
-	case Sun:
-		return "Sunday";
-	case Mon:
-		return "Monday";
-	case Tue:
-		return "Tuesday";
-	case Wed:
-		return "Wednesday";
-	case Thu:
-		return "Thursday";
-	case Fri:
-		return "Friday";
+	case Jan:
+		return "January";
+	case Feb:
+		return "Feburary";
+	case Mar:
+		return "March";
+	case Apr:
+		return "April";
+	case May:
+		return "May";
+	case Jun:
+		return "Jun";
+	case Jul:
+		return "July";
+	case Aug:
+		return "August";
+	case Sep:
+		return "September";
+	case Oct:
+		return "October";
+	case Nov:
+		return "November";
+	case Dec:
+		return "December";
 	default:
-		return "Invvalid Day";
+		return "Invalid month number";
 	}
 }
 
 int main()
 {
 
-	cout << getDayOfWeek(readDayOfWeek()) << endl;
+	cout << getMonthOfYear(readMonthOfYear()) << endl;
 
 	return 0;
 }
