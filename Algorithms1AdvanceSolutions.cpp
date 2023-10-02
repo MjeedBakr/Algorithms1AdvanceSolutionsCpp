e95756c5b0694923000e3fc1b59e7a4f3eae9c69
Abdulmajeed <s443011939@st.uqu.edu.sa>
2023-10-02 14:51:21 +0300

    46#: print cabital letters

--- Algorithms1AdvanceSolutions.cpp
+++ Algorithms1AdvanceSolutions.cpp
@@ -2,64 +2,14 @@
 #include <string>
 using namespace std;
 
-enum enMonthOfYear {Jan = 1, Feb = 2, Mar = 3, Apr = 4, May = 5,
-					Jun = 6, Jul = 7, Aug = 8, Sep = 9, Oct = 10,
-					Nov = 11, Dec = 12};
-
-int readNumberInRange(string message, int start, int end)
+void printCabitalLettersAtoZ()
 {
-	int number = 0;
-	do
-	{
-		cout << message << endl;
-		cin >> number;
-	} while (number < start || number > end);
-
-	return number;
-}
-
-enMonthOfYear readMonthOfYear()
-{
-	return (enMonthOfYear)readNumberInRange("Please enter month number 1-12: ", 1, 12);
-}
-
-string getMonthOfYear(enMonthOfYear month)
-{
-	switch (month)
-	{
-	case Jan:
-		return "January";
-	case Feb:
-		return "Feburary";
-	case Mar:
-		return "March";
-	case Apr:
-		return "April";
-	case May:
-		return "May";
-	case Jun:
-		return "Jun";
-	case Jul:
-		return "July";
-	case Aug:
-		return "August";
-	case Sep:
-		return "September";
-	case Oct:
-		return "October";
-	case Nov:
-		return "November";
-	case Dec:
-		return "December";
-	default:
-		return "Invalid month number";
-	}
+	for (int i = 65; i <= 90; i++)
+		cout << char(i) << endl;
 }
 
 int main()
 {
-
-	cout << getMonthOfYear(readMonthOfYear()) << endl;
-
+	printCabitalLettersAtoZ();
 	return 0;
 }
